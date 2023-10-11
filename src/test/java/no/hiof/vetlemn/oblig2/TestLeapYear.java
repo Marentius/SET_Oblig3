c515ef1bf09c1794f89cfe5022a3ed4fa4950cfa
marentius <vetlenilseen@hotmail.com>
2023-10-11 15:36:05 +0200

    Comitter Oblig 2 til Github repo

--- src/test/java/no/hiof/vetlemn/oblig2/TestLeapYear.java
+++ src/test/java/no/hiof/vetlemn/oblig2/TestLeapYear.java
@@ -4,24 +4,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class TestLeapYear {
     @Test
-    void leapyear_when_divisible_by_4_but_not_100() {
+    void test_leap_year_when_divisible_by_4_but_not_100() {
         assertTrue(LeapYear.isLeapYear(2024));
     }
 
     @Test
-    void leapyear_when_divisible_by_400() {
+    void test_leap_year_when_divisible_by_400() {
         assertTrue(LeapYear.isLeapYear(2000));
         assertFalse(LeapYear.isLeapYear(2100));
     }
 
     @Test
-    void not_leapyear_when_not_divisible_by_4() {
+    void test_not_leap_year_when_not_divisible_by_4() {
         assertFalse(LeapYear.isLeapYear(2023));
         assertTrue(LeapYear.isLeapYear(2024));
     }
 
     @Test
-    void not_leapyear_when_divisible_by_100_but_leap_year_when_divisible_by_400() {
+    void test_not_leap_year_when_divisible_by_100_but_leap_year_when_divisible_by_400() {
         assertFalse(LeapYear.isLeapYear(1900));
     }
 }
